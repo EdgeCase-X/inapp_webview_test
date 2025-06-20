5df43252c2f9308a46b5586a1c744b70f6c34a42
Jorge Pabón <J-Pabon@users.noreply.github.com>
2025-06-20 09:21:27 -0500

    Add example classes for inapp_webview plugin

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,152 @@
+import 'package:flutter/foundation.dart'
+    show defaultTargetPlatform, kDebugMode, kIsWeb;
 import 'package:flutter/material.dart';
+import 'package:flutter_inappwebview/flutter_inappwebview.dart'
+    show
+        InAppLocalhostServer,
+        InAppWebViewController,
+        WebViewEnvironment,
+        WebViewEnvironmentSettings;
+import 'package:inapp_webview_test/example/chrome_safari_browser_example.screen.dart' show ChromeSafariBrowserExampleScreen;
+import 'package:inapp_webview_test/example/headless_in_app_webview.screen.dart' show HeadlessInAppWebViewExampleScreen;
+import 'package:inapp_webview_test/example/in_app_browser_example.screen.dart' show InAppBrowserExampleScreen;
+import 'package:inapp_webview_test/example/in_app_webiew_example.screen.dart' show InAppWebViewExampleScreen;
+import 'package:inapp_webview_test/example/web_authentication_session_example.screen.dart' show WebAuthenticationSessionExampleScreen;
+import 'package:pointer_interceptor/pointer_interceptor.dart' show PointerInterceptor;
 
-void main() {
+final localhostServer = InAppLocalhostServer(documentRoot: 'assets');
+WebViewEnvironment? webViewEnvironment;
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (!kIsWeb && defaultTargetPlatform == TargetPlatform.windows) {
+    final availableVersion = await WebViewEnvironment.getAvailableVersion();
+    assert(
+      availableVersion != null,
+      'Failed to find an installed WebView2 runtime or non-stable Microsoft Edge installation.',
+    );
+
+    webViewEnvironment = await WebViewEnvironment.create(
+      settings: WebViewEnvironmentSettings(userDataFolder: 'custom_path'),
+    );
+  }
+
+  if (!kIsWeb && defaultTargetPlatform == TargetPlatform.android) {
+    await InAppWebViewController.setWebContentsDebuggingEnabled(kDebugMode);
+  }
+  
   runApp(const MyApp());
 }
+
+PointerInterceptor myDrawer({required BuildContext context}) {
+  var children = [
+    ListTile(
+      title: Text('InAppWebView'),
+      onTap: () {
+        Navigator.pushReplacementNamed(context, '/');
+      },
+    ),
+    ListTile(
+      title: Text('InAppBrowser'),
+      onTap: () {
+        Navigator.pushReplacementNamed(context, '/InAppBrowser');
+      },
+    ),
+    ListTile(
+      title: Text('ChromeSafariBrowser'),
+      onTap: () {
+        Navigator.pushReplacementNamed(context, '/ChromeSafariBrowser');
+      },
+    ),
+    ListTile(
+      title: Text('WebAuthenticationSession'),
+      onTap: () {
+        Navigator.pushReplacementNamed(context, '/WebAuthenticationSession');
+      },
+    ),
+    ListTile(
+      title: Text('HeadlessInAppWebView'),
+      onTap: () {
+        Navigator.pushReplacementNamed(context, '/HeadlessInAppWebView');
+      },
+    ),
+  ];
+  if (kIsWeb) {
+    children = [
+      ListTile(
+        title: Text('InAppWebView'),
+        onTap: () {
+          Navigator.pushReplacementNamed(context, '/');
+        },
+      ),
+    ];
+  } else if (defaultTargetPlatform == TargetPlatform.macOS) {
+    children = [
+      ListTile(
+        title: Text('InAppWebView'),
+        onTap: () {
+          Navigator.pushReplacementNamed(context, '/');
+        },
+      ),
+      ListTile(
+        title: Text('InAppBrowser'),
+        onTap: () {
+          Navigator.pushReplacementNamed(context, '/InAppBrowser');
+        },
+      ),
+      ListTile(
+        title: Text('WebAuthenticationSession'),
+        onTap: () {
+          Navigator.pushReplacementNamed(context, '/WebAuthenticationSession');
+        },
+      ),
+      ListTile(
+        title: Text('HeadlessInAppWebView'),
+        onTap: () {
+          Navigator.pushReplacementNamed(context, '/HeadlessInAppWebView');
+        },
+      ),
+    ];
+  } else if (defaultTargetPlatform == TargetPlatform.windows ||
+      defaultTargetPlatform == TargetPlatform.linux) {
+    children = [
+      ListTile(
+        title: Text('InAppWebView'),
+        onTap: () {
+          Navigator.pushReplacementNamed(context, '/');
+        },
+      ),
+      ListTile(
+        title: Text('InAppBrowser'),
+        onTap: () {
+          Navigator.pushReplacementNamed(context, '/InAppBrowser');
+        },
+      ),
+      ListTile(
+        title: Text('HeadlessInAppWebView'),
+        onTap: () {
+          Navigator.pushReplacementNamed(context, '/HeadlessInAppWebView');
+        },
+      ),
+    ];
+  }
+  return PointerInterceptor(
+    child: Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            decoration: BoxDecoration(color: Colors.blue),
+            child: Text('flutter_inappwebview example'),
+          ),
+          ...children,
+        ],
+      ),
+    ),
+  );
+}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -13,21 +157,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -37,15 +166,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -58,65 +178,53 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    if (kIsWeb) {
+      return MaterialApp(
+        initialRoute: '/',
+        routes: {'/': (context) => InAppWebViewExampleScreen()},
+      );
+    }
+    if (defaultTargetPlatform == TargetPlatform.macOS) {
+      return MaterialApp(
+        initialRoute: '/',
+        routes: {
+          '/': (context) => InAppWebViewExampleScreen(),
+          '/InAppBrowser': (context) => InAppBrowserExampleScreen(),
+          '/HeadlessInAppWebView':
+              (context) => HeadlessInAppWebViewExampleScreen(),
+          '/WebAuthenticationSession':
+              (context) => WebAuthenticationSessionExampleScreen(),
+        },
+      );
+    } else if (defaultTargetPlatform == TargetPlatform.windows ||
+        defaultTargetPlatform == TargetPlatform.linux) {
+      return MaterialApp(
+        initialRoute: '/',
+        routes: {
+          '/': (context) => InAppWebViewExampleScreen(),
+          '/InAppBrowser': (context) => InAppBrowserExampleScreen(),
+          '/HeadlessInAppWebView':
+              (context) => HeadlessInAppWebViewExampleScreen(),
+        },
+      );
+    }
+    return MaterialApp(
+      initialRoute: '/',
+      routes: {
+        '/': (context) => InAppWebViewExampleScreen(),
+        '/InAppBrowser': (context) => InAppBrowserExampleScreen(),
+        '/ChromeSafariBrowser': (context) => ChromeSafariBrowserExampleScreen(),
+        '/HeadlessInAppWebView':
+            (context) => HeadlessInAppWebViewExampleScreen(),
+        '/WebAuthenticationSession':
+            (context) => WebAuthenticationSessionExampleScreen(),
+      },
     );
   }
 }
