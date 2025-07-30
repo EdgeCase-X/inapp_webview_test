8e3e86b98ca5e3958fff467a916863331a493a3f
Jorge Pabón <J-Pabon@users.noreply.github.com>
2025-07-30 11:11:16 -0400

    Remove unused objects

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/foundation.dart' show kDebugMode;
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart'
-    show InAppLocalhostServer, InAppWebViewController, WebViewEnvironment;
+    show InAppWebViewController, WebViewEnvironment;
 import 'package:inapp_webview_test/pages/main_page.dart' show MainPage;
 
-final localhostServer = InAppLocalhostServer(documentRoot: 'assets');
 WebViewEnvironment? webViewEnvironment;
 
 Future main() async {
